7a0d4b35dbc6138a81c9f3e0230737b079e5dcee
pedroaugustoferreira <43154432+pedroaugustoferreira@users.noreply.github.com>
2019-12-30 15:05:04 -0300

    Update etc_backup.sh

--- backup/all/etc_backup.sh
+++ backup/all/etc_backup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -l
+# mkdir /backup
+# lvcreate -L+1G --name backup vg00
+# mkfs.ext4 /dev/vg00/backup
+# /dev/mapper/vg00-backup    /backup                    ext4    defaults        1 2
+# mount /backup
+# cd /backup
+
+
 # 0 1 1,15 * * /backup/etc/etc_backup.sh  &> /backup/etc/etc_backup.log
+
+
 # https://docs.openshift.com/container-platform/3.11/day_two_guide/environment_backup.html#creating-master-backup_environment-backup
 
 . /root/.bashrc
