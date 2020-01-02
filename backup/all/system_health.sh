60b8a024953be0ff21f28a6cc83a3ab427fd4e71
pedroaugustoferreira <43154432+pedroaugustoferreira@users.noreply.github.com>
2020-01-02 09:37:59 -0300

    Update system_health.sh

--- backup/all/system_health.sh
+++ backup/all/system_health.sh
@@ -1,6 +1,5 @@
 # mkdir /backup/system_health;cd /backup/system_health;pwd
-# wget --no-check-certificate https://raw.githubusercontent.com/pedroaugustoferreira/ocp/master/backup/all/system_health.sh
-# chmod +x system_health.sh
+# wget --no-check-certificate https://raw.githubusercontent.com/pedroaugustoferreira/ocp/master/backup/all/system_health.sh;chmod +x system_health.sh;ls -la
 
 ## backup config
 # 0 1 1,15 * * /backup/etc/etc_backup.sh           &> /backup/etc/etc_backup.log
