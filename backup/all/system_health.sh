5afc5efe5a991ea5035068532da348ecae1253f8
pedroaugustoferreira <43154432+pedroaugustoferreira@users.noreply.github.com>
2020-01-02 10:50:11 -0300

    Update system_health.sh

--- backup/all/system_health.sh
+++ backup/all/system_health.sh
@@ -1,3 +1,7 @@
+#!/bin/bash -l
+cd /backup/system_health
+file=$(date "+%d%m")
+
 # mkdir /backup/system_health;cd /backup/system_health;pwd
 # wget --no-check-certificate https://raw.githubusercontent.com/pedroaugustoferreira/ocp/master/backup/all/system_health.sh;chmod +x system_health.sh;ls -la
 
@@ -5,10 +9,6 @@
 #0 1 1,15 * * /backup/etc/etc_backup.sh           &> /backup/etc/etc_backup.log
 #0 2 * * * /backup/system_health/system_health.sh &> /backup/system_health/system_health.log
 #* * * * * /backup/system_health/system_health.sh &> /backup/system_health/system_health.log
-
-#!/bin/bash -l 
-cd /backup/system_health
-file=$(date "+%d%m")
 
 netstat -nlp   &> ${file}.netstat.log
                
